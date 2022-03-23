d3a7cedc3a31ab0374a1a83105b4b69636a1ef03
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-03-23 21:56:19 +0200

    Add delete all articles

--- app.js
+++ app.js
@@ -55,6 +55,17 @@ app.post("/articles", function (req, res) {
     });
 });
 
+// DELETE All Articles
+app.delete("/articles", function (req, res) {
+    Article.deleteMany({}, function (err) {
+        if (!err) {
+            res.send("Successfully deleted all records!");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
